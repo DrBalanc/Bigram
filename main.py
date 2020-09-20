41dd91ffe7bb09e9a0096751004b348fcf5b4227
DrBalanc <71587367+DrBalanc@users.noreply.github.com>
2020-09-20 12:56:28 +0300

    Add Programm

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import argparse
+
+def educ(text):
+    dict = {}
+    s = ""
+    ls = ""
+    for i in text:
+        for j in i:
+            if j == ' ' or j == '\n' or j == ',' or j == '.':
+               if ls != "":
+                  try:
+                     tmp = dict.get(ls)
+                     tmp.append(s)
+                     dict.update({ls + " " + s : tmp})
+                  except Exception:
+                     dict.update({ls : [s]})
+               ls = s
+               s = ""
+            else:
+                if 'а' <= j <= 'я' or 'А' <= j <= 'Я':
+                    s += j
+    return dict
+
+def gen(n, dict):
+    now = random.choice(list(dict.keys()))
+    text = open("final.txt", "w", encoding="utf8")
+    for i in range(n):
+        next = random.choice(dict.get(now))
+        text.write(next + " ")
+        now = next
+    text.close()
+
+def read_cmd():
+    importer = argparse.ArgumentParser(description='биграмка')
+    importer.add_argument('n', type=int, help='Введите размер поля')
+    args = importer.parse_args()
+    return args.n
+
+text = open("Input.txt", "r", encoding="utf8")
+dict = educ(text)
+gen(read_cmd(), dict)
+text.close()
